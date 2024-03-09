22775f4eed71a65797df61b406f90dd45f5fcd44
Rodolph Van Romondt <mr.rodolph@gmail.com>
2024-03-08 16:09:21 -0800

    POST /items route

--- routes.js
+++ routes.js
@@ -9,5 +9,25 @@ router.get("/", function (req, res) {
     return res.json(items);
 });
 
+/* POST /items: add an items to the list. */
+router.post("/", function (req, res) {
+
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({message: "Incorrect Request."})
+    }
+
+    const inOUt = items.filter(function (ele) {
+        return ele.name === req.body.name;
+    });
+
+    if (!inOUt.length) {
+        items.push(req.body);
+    } else {
+        return res.status(400).json({message: "Item is already in shopping list."});
+    }
+    
+    return res.status(201).json({ added: req.body});
+});
+
 
 module.exports = router;
